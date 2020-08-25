3cceac5451f5d04cca6b206f33ec05d23dcc0fc0
Zach Smith <zach@commure.com>
2020-08-24 19:03:18 -0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6ba47bf98433e11d8a37f0f065f91612'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '83c6d4e1-a111-4f09-aadb-9feb4ad55c80'
+};
